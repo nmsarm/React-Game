7117812b9866c57d001e7216f24621e931934dea
dumbneko <emdeeel0306@gmail.com>
2022-03-15 11:42:23 +0800

    BACKGROUND

---
+++ src/Pages/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Styles/HomeScreenStyle.scss';
+
+function HomeScreen() {
+    return(
+        <>
+            <p>SHET</p>
+        </>
+    );
+}
+
+export default HomeScreen;
