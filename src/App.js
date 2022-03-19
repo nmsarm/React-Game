05b6eb25305fdb82573287d2681235054ca7422c
nmsarm <daneah.sarmiento@yahoo.com>
2022-03-19 09:58:42 +0800

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from './Pages/HomeScreen';
-/* import MainGame from './Pages/MainGame';
-import GameOver from './Pages/GameOver';
-import Congratulations from './Pages/Congratulations'; */
+import MainGame from './Pages/MainGame';
+// import GameOver from './Pages/GameOver';
+// import Congratulations from './Pages/Congratulations'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HomeScreen />
-        
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/main-game" element={<MainGame />} />
+        </Routes>
+      </BrowserRouter>
       </header>
     </div>
   );
--- src/Pages/HomeScreen.js
+++ src/Pages/HomeScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import '../Styles/HomeScreenStyle.scss';
 import '../Styles/ButtonStyle.scss';
 
@@ -8,7 +10,11 @@ function HomeScreen() {
             <div className="container">
                 <p className="title">AXIE MEMORY GAME</p>
                 <p className="description"> BITCH NA PEKE ANG DAMI MONG DADA</p>
-                <button className="playBtn">Play</button>
+                <Link to ="/main-game" aria-current="page">
+                    <button className="playBtn">
+                        Play
+                    </button>
+                </Link>
             </div>
         </>
     );
