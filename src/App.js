45c7a80218c84a31fd7452ba14f16eb6506fa804
nmsarm <daneah.sarmiento@yahoo.com>
2022-03-26 11:15:33 +0800

    update for github pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/main-game" element={<MainGame />} />
